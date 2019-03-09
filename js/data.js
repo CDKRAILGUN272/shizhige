63b0166d8446b97f2b8ed19f270ce25228381816
CDKRAILGUN272 <cdk272.outlook.com>
2019-03-09 13:29:26 +0800

    first

---
+++ js/data.js
@@ -0,0 +1,279 @@
+﻿window.data = {
+    "pools": {
+        "event_20160411": {
+            "name": "贞德[Alter]推荐召唤",
+            "banner": "img/banners/event-20160411.png",
+            "enable": 1,
+
+            "servant_ssr_up": [
+                {
+                    "id": "002",
+                    "rate": 0.04
+                },
+				 {
+                    "id": "006",
+                    "rate": 0.04
+                },
+                {
+                    "id": "009",
+                    "rate": 0.04
+                },
+                {
+                    "id": "010",
+                    "rate": 0.04
+                },
+				 {
+                    "id": "011",
+                    "rate": 0.04
+                },
+                {
+                    "id": "014",
+                    "rate": 0.04
+                },
+                {
+                    "id": "015",
+                    "rate": 0.04
+                },
+				 {
+                    "id": "018",
+                    "rate": 0.04
+                },
+                {
+                    "id": "019",
+                    "rate": 0.04
+                },
+                {
+                    "id": "020",
+                    "rate": 0.04
+                },
+				 {
+                    "id": "022",
+                    "rate": 0.04
+                },
+           
+            ],
+
+            "servant_sr_up": [],
+
+            "servant_r_up": [],
+
+            "craft_essence_ssr_up": [],
+
+            "craft_essence_sr_up": [
+                {
+                    "id": "000",
+                    "rate": 0.08
+                },
+                {
+                    "id": "000",
+                    "rate": 0.08
+                },
+                {
+                    "id": "000",
+                    "rate": 0.08
+                },
+            ],
+
+            "craft_essence_r_up": [],
+			"servant_ssrb": [
+                "002","006","009","010",
+                "011","014","015","018","019","020","022"
+            ],
+            "servant_ssr": [
+                "001","002","003","004","005","006","007","008","009","010",
+                "011","012","013","014",,"015","016","017","018","019","020","021","022","023","024","025","026","027","028","029"
+            ],
+
+            "servant_sr": [
+                "000"
+            ],
+
+            "servant_r": [
+                "099"
+            ],
+
+            "craft_essence_ssr": [
+                "002","006","009","010","011","014","015","018","019","020",
+                "022"
+            ],
+
+            "craft_essence_sr": [
+                "000"
+            ],
+
+            "craft_essence_r": [
+                "099"
+            ],
+        },
+
+       "event_summer_1": {
+            "name": "夏日推荐召唤1",
+            "banner": "img/banners/event-20160831-1.png",
+            "enable": 1,
+
+             "servant_ssr_up": [
+                {
+                    "id": "014",
+                    "rate": 0.68
+                },
+            ],
+
+            "servant_sr_up": [],
+
+            "servant_r_up": [],
+
+            "craft_essence_ssr_up": [],
+
+            "craft_essence_sr_up": [
+                {
+                    "id": "000",
+                    "rate": 0
+                },
+                {
+                    "id": "000",
+                    "rate": 0
+                },
+                {
+                    "id": "000",
+                    "rate": 0
+                },
+            ],
+
+            "craft_essence_r_up": [],
+
+            "servant_ssr": [
+                "001","002","003","004","005","006","007","008","009","010",
+                "011","012","013","014","015","016","017","018","019","020","021","022","023","024","025","026","027","028","029"
+            ],
+
+            "servant_sr": [
+                "000"
+            ],
+
+            "servant_r": [
+                "099"
+            ],
+
+            "craft_essence_ssr": [
+                "002","006","009","010","011","014","015","018","019","020",
+                "022"
+            ],
+
+            "craft_essence_sr": [
+                "000"
+            ],
+
+            "craft_essence_r": [
+                "099"
+            ],
+        },
+
+       "event_summer_2": {
+            "name": "夏日推荐召唤2",
+            "banner": "img/banners/event-20160831-2.png",
+            "enable": 1,
+            "servant_ssr_up": [
+                {
+                    "id": "014",
+                    "rate": 0.68
+                },
+            ],
+
+            "servant_sr_up": [],
+
+            "servant_r_up": [],
+
+            "craft_essence_ssr_up": [],
+
+            "craft_essence_sr_up": [
+                {
+                    "id": "000",
+                    "rate": 0.08
+                },
+                {
+                    "id": "000",
+                    "rate": 0.08
+                },
+                {
+                    "id": "000",
+                    "rate": 0.08
+                },
+            ],
+
+            "craft_essence_r_up": [],
+
+            "servant_ssr": [
+                "001","002","003","004","005","006","007","008","009","010",
+                "011","012","013","014",,"015","016","017","018","019","020","021","022","023","024","025","026","027","028","029"
+            ],
+
+            "servant_sr": [
+                "000"
+            ],
+
+            "servant_r": [
+                "099"
+            ],
+
+            "craft_essence_ssr": [
+                "002","006","009","010","011","014","015","018","019","020",
+                "022"
+            ],
+
+            "craft_essence_sr": [
+                "000"
+            ],
+
+            "craft_essence_r": [
+                "099"
+            ],
+        },
+        "event_mock": {
+            "name": "卡池信息模板",
+            "banner": "img/banners/mock.png",
+            "enable": 0,
+            "servant_ssr_up": [
+                {
+                    "id": "000",
+                    "rate": 0.68
+                }
+            ],
+            "servant_sr_up": [
+                {
+                    "id": "000",
+                    "rate": 0.5
+                }
+            ],
+            "servant_r_up": [
+                {
+                    "id": "000",
+                    "rate": 0.25
+                }
+            ],
+            "craft_essence_ssr_up": [
+                {
+                    "id": "000",
+                    "rate": 0.25
+                }
+            ],
+            "craft_essence_sr_up": [
+                {
+                    "id": "000",
+                    "rate": 0.333333
+                }
+            ],
+            "craft_essence_r_up": [
+                {
+                    "id": "000",
+                    "rate": 0.25
+                }
+            ],
+            "servant_ssr": ["000"],
+            "servant_sr": ["000"],
+            "servant_r": ["000"],
+            "craft_essence_ssr": ["000"],
+            "craft_essence_sr": ["000"],
+            "craft_essence_r": ["000"]
+        }
+    }
+}
